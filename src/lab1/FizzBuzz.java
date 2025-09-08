b8a92f918455e8e60e87c2a6d3306baf8727b8e4
Sophia Xie <ningyi.xie@mail.utoronto.ca>
2025-09-08 18:10:28 -0400

    asd1

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -23,32 +23,6 @@ class FizzBuzz {
                     System.out.println(i);
                 }
             }
-
-            for (i = 1; i < 100; i++) {
-
-                // Find out which numbers divide i.
-                boolean divisibleBy3 = i % 3 == 0;
-                boolean divisibleBy5 = i % 5 == 0;
-
-                // Print our appropriate result.
-                if (divisibleBy3 && divisibleBy5) {
-
-                    System.out.println("Fizz Buzz");
-
-                } else if (divisibleBy3) {
-
-                    System.out.println("Fizz");
-
-                } else if (divisibleBy5) {
-
-                    System.out.println("Buzz");
-
-                } else {
-
-                    System.out.println(i);
-
-                }
-            }
         }
     }
 }
