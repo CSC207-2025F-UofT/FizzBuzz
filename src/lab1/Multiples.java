dd1cdfbea37ad2928bb3e8c25a0ecbae65827862
Sophia Xie <ningyi.xie@mail.utoronto.ca>
2025-09-08 18:57:45 -0400

    ads2

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -1,9 +1,19 @@
-//package lab1;
+package lab1;
 
-//public class Multiples {
-//    public static void main(String[] args) {
- //      int count = multiples(1000, 3, 5);
- //        System.out.println(count);
- //   }
-
-//}
+public class Multiples {
+    public static void main(String[] args) {
+        System.out.println(multiples());
+    }
+    public static int multiples(int n, int a, int b) {
+        int count = 0;
+        for (int i = 1; i < n; i++) {
+            if (i % a == 0 || i % b == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+    public static int multiples() {
+        return multiples(1000, 3, 5);
+    }
+}
