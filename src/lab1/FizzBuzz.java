83365a9db0512dbff5e6d3b73a971911948141a5
Lucy-1123 <lucyluxi.wang@mail.utoronto.ca>
2025-09-09 12:18:12 -0400

    refactored to extract helper method

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -7,6 +7,10 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
+        doFizzBuzz();
+    }
+
+    public static void doFizzBuzz() {
         int i = 1;
         while (i < 100){
             boolean divisibleBy3 = i % 3 == 0;
