8788d630d7715d3a5cc5f061674fc9dcfe001426
Wanru Cheng <wanru.cheng@mail.utoronto.ca>
2025-09-10 23:08:05 -0400

    final

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -1,7 +1,7 @@
 package lab1;
 public class Multiples {
 
-    // Generalized method: count numbers < n divisible by a or b
+    // General version with parameters
     public static int multiples(int n, int a, int b) {
         int count = 0;
         for (int i = 1; i < n; i++) {
@@ -12,8 +12,17 @@ public class Multiples {
         return count;
     }
 
+    // Overloaded version: no parameters, defaults to (1000, 3, 5)
+    public static int multiples() {
+        return multiples(1000, 3, 5);
+    }
+
     public static void main(String[] args) {
-        // main stays one line (inlined variable)
-        System.out.println(multiples(1000, 3, 5));
+        // Either call the default version...
+        System.out.println(multiples());
+
+        // ...or explicitly call the generalized one:
+        // System.out.println(multiples(1000, 3, 5));
     }
 }
+
