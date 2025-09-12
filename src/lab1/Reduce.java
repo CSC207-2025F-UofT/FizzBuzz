65277e98fda6c46f8d1ad908d3cff99a98005bd0
Sophia Xie <ningyi.xie@mail.utoronto.ca>
2025-09-12 15:26:47 -0400

    ads9

--- src/lab1/Reduce.java
+++ src/lab1/Reduce.java
@@ -6,9 +6,11 @@ public class Reduce {
         int count = 0;
         for (int d = 1; d * d <= n; d++) {
             if (n % d == 0) {
-                int q = n / d;
-                if (d != 1 && d != n) count++;
-                if (q != 1 && q != n && q != d) count++;
+                if(d * d == n ){
+                    count++;
+                } else {
+                    count += 2;
+                }
             }
         }
         return count;
