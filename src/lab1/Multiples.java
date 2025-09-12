0e98959035fb6f7cd5ad4eec9e804a6132f56830
crystalhuo <crystalhuo304@gmail.com>
2025-09-12 19:00:14 -0400

    Refactor Multiples: extracted multiples(n,a,b) method

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,31 @@
+package lab1;
+
+public class Multiples {
+
+    public static void main(String[] args) {
+
+        int count = multiples(1000, 3, 5);
+        System.out.println(count);
+    }
+
+    public static int multiples(int n, int a, int b) {
+        if (a == b) {
+            int sum = 0;
+            for (int i = 1; i < n; i++) {
+                if (i % a == 0) {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+
+        int sum = 0;
+        for (int i = 1; i < n; i++) {
+            if (i % a == 0 || i % b == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
+
